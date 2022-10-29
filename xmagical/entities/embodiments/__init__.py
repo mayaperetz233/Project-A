c66dcca9d0ab30a6e19d59c179f90ef9e1bb179e
mayaperetz233 <82024463+mayaperetz233@users.noreply.github.com>
2022-10-29 20:24:24 +0300

    Update __init__.py

--- xmagical/entities/embodiments/__init__.py
+++ xmagical/entities/embodiments/__init__.py
@@ -1,5 +1,6 @@
 from .base import Embodiment, NonHolonomicEmbodiment
 from .gripper import NonHolonomicGripperEmbodiment
+from .hand import NonHolonomicHandEmbodiment
 from .poly import PolygonEmbodiment
 from .star import StarEmbodiment
 from .stick import LongstickEmbodiment, MediumstickEmbodiment, ShortstickEmbodiment
